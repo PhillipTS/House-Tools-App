19d9cfcbb80fc3aac38cb1a46e5537a6d316f444
Kolya Haisken-De New <kehai3@student.monash.edu>
2018-07-14 19:19:53 +1000

    Updated count down timer tool titles styles

--- app/src/main/java/com/kolya/housetoolsapp/Tools/Timer.java
+++ app/src/main/java/com/kolya/housetoolsapp/Tools/Timer.java
@@ -4,10 +4,14 @@ package com.kolya.housetoolsapp.Tools;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.kolya.housetoolsapp.R;
@@ -17,7 +21,11 @@ import org.w3c.dom.Text;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Timer extends Tool {
+public class Timer extends Tool implements AdapterView.OnItemSelectedListener {
+
+    private int timeUnitMultiplier = 1;
+
+    private CountDownTimer countDownTimer = null;
 
     public Timer() {
         // Required empty public constructor
@@ -35,15 +43,29 @@ public class Timer extends Tool {
 
         final Button startButton = view.findViewById(R.id.start_stopCountdown);
 
+        //spinner
+        final Spinner timeUnitsSpinner = view.findViewById(R.id.timeUnitSpinner);
+        ArrayAdapter<CharSequence> adapterTimeUnits = ArrayAdapter.createFromResource(getActivity(),
+                R.array.time_units_selection, android.R.layout.simple_spinner_item);
+        adapterTimeUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeUnitsSpinner.setAdapter(adapterTimeUnits);
+
+        timeUnitsSpinner.setOnItemSelectedListener(this);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int time = Integer.parseInt("" + timerInput.getText());
 
-                new CountDownTimer(time * 1000, 1000) {
+                if (countDownTimer != null)
+                    countDownTimer.cancel();
+
+                Log.i("" + timeUnitMultiplier, "TIMER");
+
+                countDownTimer = new CountDownTimer(time*timeUnitMultiplier* 1000, timeUnitMultiplier*1000) {
 
                     public void onTick(long millisUntilFinished) {
-                        countdown.setText("" + millisUntilFinished / 1000);
+                        countdown.setText("" + (1+ millisUntilFinished / 1000/ timeUnitMultiplier));
                     }
 
                     public void onFinish() {
@@ -56,4 +78,19 @@ public class Timer extends Tool {
         return view;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.i("" + i,"ipos");
+        switch(i) {
+            case 0: timeUnitMultiplier = 1; break;
+            case 1: timeUnitMultiplier = 60; break;
+            case 2: timeUnitMultiplier = 3600; break;
+            case 3: timeUnitMultiplier = 86400; break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
